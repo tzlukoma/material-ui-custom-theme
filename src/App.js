7a3c99b2053b41476b838840dc5694809357c911
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2020-11-12 22:29:08 -0500

    Initial commit - theme set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import AppHeader from './components/AppHeader'
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    paddingTop: 20,
+    margin: 'auto'
+  }
+}))
+
+const Types = () => {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container maxWidth='sm'>
+        <Typography variant='h1' component='h2' gutterBottom>
+          h1. Heading
+        </Typography>
+        <Typography variant='h2' gutterBottom>
+          h2. Heading
+        </Typography>
+        <Typography variant='h3' gutterBottom>
+          h3. Heading
+        </Typography>
+        <Typography variant='h4' gutterBottom>
+          h4. Heading
+        </Typography>
+        <Typography variant='h5' gutterBottom>
+          h5. Heading
+        </Typography>
+        <Typography variant='h6' gutterBottom>
+          h6. Heading
+        </Typography>
+        <Typography variant='subtitle1' gutterBottom>
+          subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Quos blanditiis tenetur
+        </Typography>
+        <Typography variant='subtitle2' gutterBottom>
+          subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Quos blanditiis tenetur
+        </Typography>
+        <Typography variant='body1' gutterBottom>
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant='body2' gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography variant='button' display='block' gutterBottom>
+          button text
+        </Typography>
+        <Typography variant='caption' display='block' gutterBottom>
+          caption text
+        </Typography>
+        <Typography variant='overline' display='block' gutterBottom>
+          overline text
+        </Typography>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+function App () {
+  return (
+    <div className='App'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <AppHeader />
+        </CssBaseline>
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#bcaaa4' },
+    secondary: { main: '#ffe57f' }
+  },
+  typography: {
+    fontFamily: 'Raleway, sans-serif',
+    h1: {
+      fontFamily: 'Poppins',
+      fontWeight: 100
+    },
+    h2: {
+      fontFamily: 'Poppins',
+      fontWeight: 200
+    },
+    h3: {
+      fontFamily: 'Poppins',
+      fontWeight: 200
+    }
+  }
+})
+export default theme
