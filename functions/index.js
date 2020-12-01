8f9af77e847316445bfb399635dc2c96735db1b7
Thomas Z Lukoma <tzlukoma@morethanahut.com>
2020-12-01 08:53:49 -0500

    BKCN-2 Adds book creation email

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,11 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default
+const sendGridMail = require('@sendgrid/mail')
+const MAIL_API_KEY = functions.config().sendgrid.key
+const MAIL_TEMPLATE = functions.config().sendgrid.template
+
+sendGridMail.setApiKey(MAIL_API_KEY)
 
 admin.initializeApp()
 
@@ -24,6 +29,17 @@ const createWCProduct = async product => {
   }
 }
 
+const updateWCProduct = async (wcId, updates) => {
+  try {
+    const res = await api.put(`products/${wcId}`, updates)
+    console.log(res.data)
+    return res.data
+  } catch (err) {
+    console.log(err.response.data.message)
+    return err.response.data.message
+  }
+}
+
 const createProfile = (userRecord, context) => {
   const { email, phoneNumber, uid, displayName, photoURL } = userRecord
 
@@ -32,6 +48,28 @@ const createProfile = (userRecord, context) => {
     .doc(uid)
     .set({ email, phoneNumber, displayName, photoURL })
     .catch(console.error)
+}
+
+const sendEmail = (book, wcLink) => {
+  const msg = {
+    to: 'tzlukoma@gmail.com',
+    from: 'bookstore@brownkidsread.org',
+    templateId: MAIL_TEMPLATE,
+    dynamic_template_data: {
+      name: 'BKR Book Inventory Mgt',
+      title: book.title,
+      author: book.author,
+      amountBought: book.amountBought,
+      coverImage: book.coverImage,
+      format: book.format,
+      regularPrice: book.regularPrice,
+      description: book.description,
+      productLink: wcLink
+    },
+    hideWarnings: true // now the warning won't be logged
+  }
+
+  return sendGridMail.send(msg)
 }
 
 exports.authOnCreate = functions.auth.user().onCreate(createProfile)
@@ -58,6 +96,7 @@ exports.createWoocommerceBook = functions.firestore
       images: [{ src: book.coverImage }],
       manage_stock: true,
       stock_quantity: book.stockCount,
+      regular_price: book.regularPrice,
       sku: book.sku
     }
 
@@ -65,8 +104,19 @@ exports.createWoocommerceBook = functions.firestore
 
     try {
       const wcBookCreated = await createWCProduct(wcBody)
+      // const wcBookUpdated = await updateWCProduct(wcBookCreated.id, {
+      //   shipping_required: true,
+      //   weight: book.weight,
+      //   dimensions: {
+      //     length: book.length,
+      //     width: book.width,
+      //     height: book.height
+      //   }
+      // })
 
+      // console.log(`wcBookUpdated`, wcBookUpdated)
       console.log(`bookId`, bookId)
+      console.log(`wcPermaLink`, wcBookCreated.permalink)
       console.log(`wcBookId: `, wcBookCreated.id)
 
       const bookRef = db.collection('books').doc(bookId)
@@ -74,14 +124,31 @@ exports.createWoocommerceBook = functions.firestore
       bookRef
         .set(
           {
-            woocommerceId: wcBookCreated.id
+            woocommerceId: wcBookCreated.id,
+            woocommercePermaLink: wcBookCreated.permalink,
+            coverImage: wcBookCreated.images[0].src
           },
           { merge: true }
         )
+        .then(() => {
+          if (process.env.NODE_ENV === 'production') {
+            return sendEmail(book, wcBookCreated.permalink)
+          } else {
+            console.log(`data sent to email:`, book)
+            console.log(`link to the website entry:`, wcBookCreated.permalink)
+            return bookId
+          }
+        })
         .catch(err => console.log(err))
     } catch (error) {
       console.log(error)
     }
 
-    return bookId
+    // if (process.env.NODE_ENV === 'production') {
+    //   return sendEmail(book, wcBookCreated.permalink)
+    // } else {
+    //   console.log(`data sent to email`, book)
+    // console.log(`link to the website entry`, wcBookCreated.permalink)
+    //   return bookId
+    // }
   })
